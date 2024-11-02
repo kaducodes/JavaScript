af5abfc1bbef7d030d1e83ad8bdbcbc176ebf768
kaducodes <kadu.codes@gmail.com>
2024-11-02 15:23:49 -0300

    resolução de exercício
    resolução do exercício proposta na aula 16 chamado 'Analisador de Números'

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Estou aprendendo a linguagem de programação JavaScript voltado ao desenvolvime
 Agora irei aprender a gerenciar os meus repositórios
 27/10/2024: aula 15: variáveis compostas
 01/11/2024: aula 16: funcoes
+02/11/2024: resolução do exercício proposto e assistir aula depois
--- aula16/analisador_de_numeros/script.js
+++ aula16/analisador_de_numeros/script.js
@@ -1,14 +1,42 @@
+let valores = [] //definir fora da função para não zerar o histórico toda vez que a função for chamada
+let soma = 0
+
 function lista() {
-    window.alert('lista')
+    // pega o número do input
     let num = window.document.getElementById('caixaNumero')
     let n = Number(num.value)
     let lista = window.document.getElementById('lista')
-    let valores = []
-    if (n < 1 && n > 100){
+    if (n < 1 || n > 100 || valores.includes(n)){ //verifica se está dentro do intervalo especificado e se já foi incluído
         window.alert('Número inválido ou já está na lista')
-    }   
+    } else {
+        let opcao = document.createElement('option')
+        opcao.text = `Valor ${n} adicionado!`
+        lista.appendChild(opcao) 
+        res.innerHTML = ''
+        valores.push(n) //adiciona novo número ao vetor
+        soma += n //adiciona novo número à soma
+    }  
+    num.value = '' //limpa o input
+    num.focus() //coloca o foco de volta no input
 }
 
 function finalizar(){
-    window.alert('finalizar')
+    if (valores.length == 0) {
+        window.alert('Adicionar valores antes de finalizar')
+    } else {
+        let res = document.getElementById('res')
+        // Ao todo temos x números cadastrados   
+        let qtd = valores.length
+        res.innerHTML += `<p>Ao todo temos ${qtd} números cadastrados</p>`
+        // O maior valor informado foi
+        valores.sort()
+        res.innerHTML += `<p>O maior valor informado foi ${valores[qtd - 1]}</p>`
+        // O menor valor informado foi
+        res.innerHTML += `<p>O menor valor informado foi ${valores[0]}</p>`
+        // Somando todos os valores, temos x
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        // A média dos valores digitados é x
+        let media = soma / qtd
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+    }
 }
